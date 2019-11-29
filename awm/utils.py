f4b46d9e976556f8e1f7e43e8d28c61821e3547e
Christoph Sieghart <sigi@0x2a.at>
2019-11-29 09:49:41 +0100

    Refactor StateSavingMixin
    Add stamp keyword argument to support loading/saving more than
    one state

--- awm/utils.py
+++ awm/utils.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 import torch
 
+from . import logger
+
 
 def spread(n, number_of_bins):
     """ Split *n* into *number_of_bins* almost equally sized numbers. Do not
@@ -17,21 +19,34 @@ def spread(n, number_of_bins):
 
 
 class StateSavingMixin:
-    def load_state(self, game):
+    def _build_filename(self, game, stamp):
+        if stamp is None:
+            filename = "{}-{}.torch".format(game, self.__class__.__name__.lower())
+        else:
+            filename = "{}-{}-{}.torch".format(
+                game, self.__class__.__name__.lower(), stamp
+            )
+        return filename
+
+    def load_state(self, game, stamp=None):
         # If there is a state file - load it
         device = "cpu"
         models_dir = Path("models")
-        state_file = models_dir / Path(
-            "{}-{}.torch".format(game, self.__class__.__name__.lower())
-        )
+        state_file = models_dir / Path(self._build_filename(game, stamp))
         if state_file.is_file():
+            logger.info(
+                "%s: Loading state for %s with stamp %s",
+                self.__class__.__name__,
+                game,
+                stamp,
+            )
             self.load_state_dict(torch.load(str(state_file), map_location=device))
 
-    def save_state(self, game):
+    def save_state(self, game, stamp=None):
+        logger.info(
+            "%s: Saving state for %s with stamp %s", self.__class__.__name__, game, stamp
+        )
         models_dir = Path("models")
         models_dir.mkdir(parents=True, exist_ok=True)
-
-        state_file = models_dir / Path(
-            "{}-{}.torch".format(game, self.__class__.__name__.lower())
-        )
+        state_file = models_dir / Path(self._build_filename(game, stamp))
         torch.save(self.state_dict(), str(state_file))
